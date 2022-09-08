17cd5b150270eff83ed0d0f73abc2538f9c89a3b
Mattias Lundh <mattiasanderslundh@gmail.com>
2022-09-09 00:27:51 +0200

    events

---
+++ sandbox/sandbox/Events/Phone.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sandbox.Events
+{
+    public class Phone
+    {
+        public event EventHandler Call;
+        public event EventHandler<TextMessageContent> TextMessage;
+
+
+        // a very simple event being broadcasted to all subscribers
+        public void OnCall()
+        {
+            Call.Invoke(this, EventArgs.Empty);
+        }
+
+        // an event containing a parameter being broadcasted to all subscribers
+        public void OnTextMessage(string text)
+        {
+            var message = new TextMessageContent();
+            message.Text = text;
+
+            TextMessage.Invoke(this, message);
+        }
+    }
+}
---
+++ sandbox/sandbox/Program.cs
@@ -0,0 +1,22 @@
+﻿using sandbox.Events;
+
+var phone = new Phone();
+
+// the  +=  syntax is used to subscribe a function to an event. This function will run when the event occurs.
+// you will need to put 'sender' and 'eventArgs' here because thats what is required.
+phone.Call += (sender, eventArgs) =>
+{
+    Console.WriteLine("Moshi moshi, gaijin des");
+};
+
+phone.TextMessage+= (sender, textMessageContent) =>
+{
+    Console.WriteLine($"oh, i got a new text, it reads: {textMessageContent.Text}");
+};
+
+
+// this activates the events;
+phone.OnCall();
+phone.OnTextMessage("dinner starts at 8.00pm");
+
+Console.WriteLine();
